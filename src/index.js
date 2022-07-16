a925463fb2a0c4227e87a8bf871bf358da61c446
Haervwe <juan-jose-007@hotmail.com>
2022-07-16 00:24:08 -0300

    improve turn visual styling cue, add first implementation of a  minimax lagortith(not working)

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import "./style.scss";
-
+import _ from "lodash";
 
 const sabaGame = (()=>{
     var board = document.getElementById("board");
@@ -8,7 +8,7 @@ const sabaGame = (()=>{
     var currentPlayer = 2;
     var gameType;
     var mark = "";
-    var roundStartPlayer = 1;
+    var roundStartPlayer = 2;
 
     const PlayerFactory = (name,type) =>{
         let mark;
@@ -60,15 +60,20 @@ const sabaGame = (()=>{
             }
         }
 
-        function resetMark (){
-            this.mark = "";
-            render(this);
+        function resetMark (copy){
+            if (copy == false){
+                this.mark = "";
+                render(this);
+            }
+            if (copy == true){
+                this.mark = "";
+            }
         }
 
         function setMark (){
             let player1score = document.getElementById("player1score");
             let player2score = document.getElementById("player2score");
-            console.log("X:",this.x,"Y:",this.y,"ID:",this.id);
+            console.log(currentPlayer);
             if (this.mark == ""){
                 if  (currentPlayer == 2){
                     render(this);
@@ -80,50 +85,42 @@ const sabaGame = (()=>{
                         board.className = `player0turn`;
                     }
                     this.mark = players[currentPlayer].mark;
-                    let check = checkWinner(game,this.x,this.y);
+                    let check = checkWinner(game,this.x,this.y,false);
                     render(this);
-                    if (check.score>=0){
-                        if (currentPlayer == 0){
-                            players[0].plusScore(check.score)
-                            player1score.innerText = `Score: ${players[0].score}`;
-                            currentPlayer = 1;
-                            if (check.reset == true){
-                                resetGame();
-                            }
-                            return;
-                        }
-                        if(currentPlayer == 1){
-                            players[1].plusScore(check.score)
-                            player2score.innerText = `Score: ${players[1].score}`;
-                            currentPlayer = 0;
-                            if (check.reset == true){
-                                resetGame();
-                            }
-                            return;
-                        }
-                    }
-                    if ((gameType == "ai") && (check.reset == false)){
+                    if (currentPlayer == 0){
+                        players[0].plusScore(check.score)
+                        player1score.innerText = `Score: ${players[0].score}`;
                         currentPlayer = 1;
-                        let index = miniMax(game,0,true).index;
-                        game[index].mark = "O";
-                        players[1].plusScore(checkWinner(game,this.x,this.y).score)
-                        player2score.innerText = `Score: ${players[1].score}`;
-                        render(game[index]);
                         if (check.reset == true){
                             resetGame();
                         }
-                        currentPlayer = 0;
-                        return;
-                    } else {
-                        if (currentPlayer == 0){
-                            currentPlayer = 1;
-                        } else {
+                        if ((gameType == "ai") && (check.reset == false)){
+                            console.log("ai");
+                            let index = miniMax(game,0,true,this.x,this.y);
+                            game[index.x][index.y].mark = players[1].mark;
+                            players[1].plusScore(checkWinner(game,this.x,this.y,false).score);
+                            player2score.innerText = `Score: ${players[1].score}`;
+                            render(game[index.x][index.y]);
+                            board.className = `player0turn`;
+                            if (check.reset == true){
+                                resetGame();
+                                return;
+                            }
                             currentPlayer = 0;
+                            return;
                         }
-                        
+                        return;
+                    }
+                    if(currentPlayer == 1){
+                        players[1].plusScore(check.score)
+                        player2score.innerText = `Score: ${players[1].score}`;
+                        currentPlayer = 0;
+                        if (check.reset == true){
+                            resetGame();
+                        }
+                        return;
                     }
                     
-                    console.log(currentPlayer);
                     if(players[0].score >= 100){
                         showWinner(players[0]);
                     }
@@ -144,11 +141,92 @@ const sabaGame = (()=>{
         };
     };
 
-    function checkWinner(game,x,y){
+    //miniMax algorithm function for ai play
+
+    function miniMax (gameTemp ,depth, isMaximizer,x,y){
+
+        var gameCopy = _.cloneDeep(gameTemp);
+        let moves = [];
+    //next possible moves evaluator for AI player
+        let check = checkWinner(gameCopy,x,y,true);
+        if (check.reset == true||depth==3){
+            if(isMaximizer==true){
+                return {scoreAi: 0, scoreHuman: check.score, x: x, y: y};
+            }
+            if(isMaximizer==true){
+                return {scoreAi: check.score, scoreHuman: 0, x: x, y: y};
+            }
+        }
+        if (isMaximizer == true){
+            for (let i = 0; i < 12; i++){
+                for(let j = 0; j < 12; j++){
+                    if (gameCopy[i][j].mark == ""){
+                        gameCopy[i][j].mark = "O";
+                        let move = miniMax(gameCopy, depth +1, false,i,j);
+                        gameCopy[i].mark = "";
+                        moves.push({scoreAi: check.score + move.scoreAi, scoreHuman: move.scoreHuman, x: i, y: j});
+                    }
+                }
+            }
+        }
+        
+    //next possible moves evaluator for human player
+        if(isMaximizer == false){
+            for (let i = 0; i < 12; i++){
+                for(let j = 0; j < 12; j++){
+                    if (gameCopy[i][j].mark == ""){
+                        gameCopy[i][j].mark = "X";
+                        let move = miniMax(gameCopy, depth +1, true,i,j);
+                        gameCopy[i].mark = "";
+                        moves.push({scoreAi: move.scoreAi, scoreHuman:  check.score + move.scoreHuman, x: i, y: j});
+                    }
+                }
+            }
+        }
+            //best posible value comparison  and return the optimal value. it returns an object with the index and value of the best posible move, 
+            //the index is only relevant in depth 0 as it is the final return value of the function in every other iteration the value used for calculations is only the branch terminal score.
+            
+        if (isMaximizer == true){
+            let x = 0;
+            let y = 0;
+            let valueAi = 0;
+            let valueHuman = 0;
+            for (let i = 0 ; i < moves.length; i++){
+                if  ((moves[i].scoreAi>moves[i].scoreHuman)&&(moves[i].scoreAi >= valueAi)&&(moves[i].scoreHuman <= valueHuman)){
+                    x = moves[i].x;
+                    y = moves[i].y;
+                    valueAi = moves[i].scoreAi;
+                    valueHuman = moves[i].scoreHuman;
+                }
+            }
+            if (depth == 0){
+                console.log({scoreAi: valueAi, scoreHuman: valueHuman, x: x, y: y});
+                console.log(moves);
+            }
+            return {scoreAi: valueAi, scoreHuman: valueHuman, x: x, y: y};
+        }
+        if (isMaximizer == false){
+            let x = 0;
+            let y = 0;
+            let valueAi = 0;
+            let valueHuman = 0;
+            for (let i = 0 ; i < moves.length; i++){
+                if  ((moves[i].scoreAi<moves[i].scoreHuman)&&(moves[i].scoreAi <= valueAi)&&(moves[i].scoreHuman >= valueHuman)){
+                    x = moves[i].x;
+                    y = moves[i].y;
+                    valueAi = moves[i].scoreAi;
+                    valueHuman = moves[i].scoreHuman;
+                }
+            }
+            return {scoreAi: valueAi, scoreHuman: valueHuman, x: x, y: y};
+        }
+    }
+
+    function checkWinner(game,x,y,internal){
 
         let turnScore = 0;
         let reset = false;
-
+        
         //checks for 4 in a row
 
         if(x<9){
@@ -238,57 +316,57 @@ const sabaGame = (()=>{
         if(x<9){
             if(((game[x][y].mark != game[x+1][y].mark)&&(game[x+1][y].mark != ""))&&((game[x+1][y].mark == game[x+2][y].mark))&&((game[x][y].mark == game[x+3][y].mark))){
                 turnScore +=2;
-                game[x+1][y].resetMark();
-                game[x+2][y].resetMark();
+                game[x+1][y].resetMark(internal);
+                game[x+2][y].resetMark(internal);
             }
         }
         if(x>2){
             if(((game[x][y].mark != game[x-1][y].mark)&&(game[x-1][y].mark != ""))&&((game[x-1][y].mark == game[x-2][y].mark))&&((game[x][y].mark == game[x-3][y].mark))){
                 turnScore +=2;
-                game[x-1][y].resetMark();
-                game[x-2][y].resetMark();
+                game[x-1][y].resetMark(internal);
+                game[x-2][y].resetMark(internal);
             }
         }
         if(y<9){
             if(((game[x][y].mark != game[x][y+1].mark)&&(game[x][y+1].mark != ""))&&((game[x][y+1].mark == game[x][y+2].mark))&&((game[x][y].mark == game[x][y+3].mark))){
                 turnScore +=2;
-                game[x][y+1].resetMark();
-                game[x][y+2].resetMark();
+                game[x][y+1].resetMark(internal);
+                game[x][y+2].resetMark(internal);
             }
         }
         if(y>2){
             if(((game[x][y].mark != game[x][y-1].mark)&&(game[x][y-1].mark != ""))&&((game[x][y-1].mark == game[x][y-2].mark))&&((game[x][y].mark == game[x][y-3].mark))){
                 turnScore +=2;
-                game[x][y-1].resetMark();
-                game[x][y-2].resetMark();
+                game[x][y-1].resetMark(internal);
+                game[x][y-2].resetMark(internal);
             }
         }
         if((x<9)&&(y<9)){
             if(((game[x][y].mark != game[x+1][y+1].mark)&&(game[x+1][y+1].mark != ""))&&((game[x+1][y+1].mark == game[x+2][y+2].mark))&&((game[x][y].mark == game[x+3][y+3].mark))){
                 turnScore +=2;
-                game[x+1][y+1].resetMark();
-                game[x+2][y+2].resetMark();
+                game[x+1][y+1].resetMark(internal);
+                game[x+2][y+2].resetMark(internal);
             }
         }
         if((x>2)&&(y>2)){
             if(((game[x][y].mark != game[x-1][y-1].mark)&&(game[x-1][y-1].mark != ""))&&((game[x-1][y-1].mark == game[x-2][y-2].mark))&&((game[x][y].mark == game[x-3][y-3].mark))){
                 turnScore +=2;
-                game[x-1][y-1].resetMark();
-                game[x-2][y-2].resetMark();
+                game[x-1][y-1].resetMark(internal);
+                game[x-2][y-2].resetMark(internal);
             }
         }
         if((x>2)&&(y<9)){
             if(((game[x][y].mark != game[x-1][y+1].mark)&&(game[x-1][y+1].mark != ""))&&((game[x-1][y+1].mark == game[x-2][y+2].mark))&&((game[x][y].mark == game[x-3][y+3].mark))){
                 turnScore +=2;
-                game[x-1][y+1].resetMark();
-                game[x-2][y+2].resetMark();
+                game[x-1][y+1].resetMark(internal);
+                game[x-2][y+2].resetMark(internal);
             }
         }
         if((x<9)&&(y>2)){
             if(((game[x][y].mark != game[x+1][y-1].mark)&&(game[x+1][y-1].mark != ""))&&((game[x+1][y-1].mark == game[x+2][y-2].mark))&&((game[x][y].mark == game[x+3][y-3].mark))){
                 turnScore +=2;
-                game[x+1][y-1].resetMark();
-                game[x+2][y-2].resetMark();
+                game[x+1][y-1].resetMark(internal);
+                game[x+2][y-2].resetMark(internal);
             }
         }
 
@@ -417,7 +495,6 @@ const sabaGame = (()=>{
                 reset = true;
             }
         }
-        console.log(reset,turnScore);
         return {
             reset,
             score: turnScore,
@@ -434,7 +511,6 @@ const sabaGame = (()=>{
             }
             game.push(inner);
         }
-        console.log(game);
     }
 
     function resetGame () {
@@ -444,6 +520,9 @@ const sabaGame = (()=>{
             roundStartPlayer = 1;
         }
         if(roundStartPlayer == 1){
+            roundStartPlayer = 0;
+        }
+        if(roundStartPlayer == 2){
             roundStartPlayer = 0;
         }
         board.innerHTML = "";
